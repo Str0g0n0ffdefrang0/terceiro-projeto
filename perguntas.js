d38fcf8a94f21eae80d55e169a6a8162d54d9cf0
Laura <0000112083501xsp@al.educacao.sp.gov.br>
2024-11-27 13:59:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Promoções',
+    'Quais tipos livros estão em promoção no mẽs do dia das crianças?',
+    'Entre em The Books Kids e escolha quantos livros quer para comprar'
+)
+
+criaCartao(
+    'O pequeno príncipe',
+    'Esse é um dos livros que está em promoção no mẽs do dia das crianças',
+    'Por apenas 19,99 reais'
+)
+
+criaCartao(
+    'O mal do lobo mau',
+    'Esse é outro livro que está em promoção',
+    'Por 14,99 reais'
+)
+
+criaCartao(
+    'Cada um com o seu jeito, cada um é de um jeito',
+    'Esse é outro livro ue está em promoção',
+    'Por 14,99 reais'
+)
